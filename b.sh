aa977e9c8207cbcc9d5a88e4a46d3d26eedc0af6
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2024-02-23 07:02:03 +0530

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PWD=$(PWD)
+
+# Clone Buildroot repository
+git clone https://github.com/buildroot/buildroot.git 
+
+# Navigate to buildroot directory
+cd buildroot
+
+# Set the configuration file
+CONFIG_FILE="qemu_x86_64_x11_defconfig(1).txt"
+
+cd ..
+mv qemu_x86_64_x11_defconfig (1).txt ../buildroot
+# Load the configuration
+cd buildroot
+make qemu_x86_64_x11_defconfig (1).txt
+
+# Build the Linux distribution
+make
+
+# The generated output will be in output/images/
